78c5502187559f12448276ce2272f5876a8f295e
VincentPS <schaddelee.vincent@gmail.com>
2020-03-15 15:17:57 +0100

    - added assets - added .env configuration

--- backend/routes.php
+++ backend/routes.php
@@ -3,11 +3,14 @@
 // use ...
 
 // Get the current path and remove ending slashes
-$path = rtrim($_SERVER["REQUEST_URI"], "/");
+$path = rtrim($_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"], "/");
+
+// Remove the website root from the url
+$path = str_replace($_ENV["WEBSITE_ROOT"], "", $path);
 
 // Load the correct template
 switch ($path) {
-    case "/": // Index
+    case "": // Index
         $include_path = "public/index.php";
         $title = "[Website] - Index";
         break;
--- index.php
+++ index.php
@@ -1,4 +1,23 @@
 <?php
+
+if (file_exists('./.env')) {
+    $_ENV = parse_ini_file('./.env');
+}
+
+//if (!function_exists('env')) {
+//    function env($key, $default = null)
+//    {
+//        $value = getenv($key);
+//
+//        if ($value === false) {
+//            return $default;
+//        }
+//
+//        return $value;
+//    }
+//}
+
+
 // Require vendor
 require __DIR__ . '/vendor/autoload.php';
 
