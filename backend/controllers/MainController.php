1a2c3ed45e1d3985f74d8f67c5ebd1fa50fb6207
VincentPS <schaddelee.vincent@gmail.com>
2020-03-15 15:23:00 +0100

    - Cleaned the repo

--- backend/controllers/MainController.php
+++ backend/controllers/MainController.php
@@ -3,18 +3,10 @@
 class MainController
 {
 
-    // Paypal API context
-    public static $paypal_api_context;
-
-    // HistoryController constructor
-    public function __construct($paypal_api_context)
+    // MainController constructor
+    public function __construct()
     {
-        self::$paypal_api_context = $paypal_api_context;
+        // Add constructor functionalities here
     }
-
-    // Get the transaction history for a user
-    public static function getTransactionHistory()
-    {
-
-    }
+    
 }
